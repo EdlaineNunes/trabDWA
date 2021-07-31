9de4a122b8128f6176690540261dfc168bee47d8
Edlaine Nunes <edlaine.nunes@b2wdigital.com>
2021-07-30 23:18:54 -0300

    adjustements

--- DWAproject/settings.py
+++ DWAproject/settings.py
@@ -82,11 +82,16 @@ WSGI_APPLICATION = 'DWAproject.wsgi.application'
 
 # }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE" : "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-    }
+    'default': dj_database_url.config()
+
 }
 
 # Password validation
