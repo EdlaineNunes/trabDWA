055cb3ed40aa5550893f0d59c40f3eb6567e8af1
Edlaine Nunes <edlaine.nunes@b2wdigital.com>
2021-07-31 10:48:17 -0300

    ajustements project and app

--- DWAproject/settings.py
+++ DWAproject/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'DWAproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-
-    'default': dj_database_url.config()
-
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE" : "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-#     }
+
+#     'default': dj_database_url.config()
+
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE" : "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    }
+}
 
 
 # Password validation
